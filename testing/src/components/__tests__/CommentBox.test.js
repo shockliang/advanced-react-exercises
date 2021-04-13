cd282de84cc22bd2e62009241e04e5cc95c0c525
shockliang <shockwater@gmail.com>
2021-04-14 00:01:36 +0800

    Adding a root component.

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {mount} from 'enzyme';
 import CommentBox from "components/CommentBox";
+import Root from "Root";
 
 let wrapped;
 beforeEach(() => {
-  wrapped = mount(<CommentBox/>);
+  wrapped = mount(
+    <Root>
+      <CommentBox/>
+    </Root>);
 });
 
 afterEach(() => {
