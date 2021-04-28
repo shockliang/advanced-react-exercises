dc94be3e9211ad0239181415cab55442b779fb13
shockliang <shockwater@gmail.com>
2021-04-29 00:20:25 +0800

    Placing reusable logic.

--- testing/src/components/requireAuth.js
+++ testing/src/components/requireAuth.js
@@ -1,11 +1,30 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 
 export default (ChildComponent) => {
   class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
     render() {
       return <ChildComponent/>;
     }
   }
 
-  return ComposedComponent;
+  function mapStateToProps(state) {
+    return {auth: state.auth}
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };
