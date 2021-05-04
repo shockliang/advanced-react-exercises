8fc99f11a8d46978bb28d28ad5355e068b45d643
shockliang <shockwater@gmail.com>
2021-05-05 00:15:02 +0800

    Setup middlewares.

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
 
 const port = process.env.PORT || 3090;
 
