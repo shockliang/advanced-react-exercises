260758dc7d0c2d4b9d8b42e516d8aac7048e468c
shockliang <shockwater@gmail.com>
2021-05-06 00:50:18 +0800

    Express router handler.

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
 const app = express();
+const router = require('./router');
+
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
 const port = process.env.PORT || 3090;
 
