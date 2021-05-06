792f5623a54c73814a39dc6f5dc6d8a1c70f802a
shockliang <shockwater@gmail.com>
2021-05-07 00:15:35 +0800

    Setup mongodb.

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
