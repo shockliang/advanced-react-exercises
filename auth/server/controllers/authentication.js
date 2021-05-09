7a40294aaa5826c3a287432c84b0db780c3342d7
shockliang <shockwater@gmail.com>
2021-05-09 20:43:16 +0800

    Creating user record.

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-  res.send({success: true});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email}, function (err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    const user = new User({
+      email, password
+    });
+
+    user.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      res.json({success: true});
+    });
+  })
 }
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -7,3 +7,5 @@ const userSchema = new Schema({
 });
 
 const modelClass = mongoose.model('user', userSchema);
+
+module.exports = modelClass;
